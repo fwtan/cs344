0932d61d24df5f61d85dbbb776e0019ef637b4d3
fwtan <fuwen.tan@gmail.com>
2016-01-18 23:01:45 -0500

    solved problem set 3

--- Problem Sets/Problem Set 3/student_func.cu
+++ Problem Sets/Problem Set 3/student_func.cu
@@ -81,6 +81,169 @@
 
 #include "utils.h"
 
+__global__ void find_min_kernel(const float* const d_in, float* d_out)
+{
+    // sdata is allocated in the kernel call: 3rd arg to <<<b, t, shmem>>>
+    extern __shared__ float sdata[];
+
+    int myId = threadIdx.x + blockDim.x * blockIdx.x;
+    int tid  = threadIdx.x;
+
+    // load shared mem from global mem
+    sdata[tid] = d_in[myId];
+    __syncthreads();            // make sure entire block is loaded!
+
+    // do reduction in shared mem
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+    {
+        if (tid < s)
+        {
+            sdata[tid] = min(sdata[tid], sdata[tid + s]);
+        }
+        __syncthreads();        // make sure all adds at one stage are done!
+    }
+
+    // only thread 0 writes result for this block back to global mem
+    if (tid == 0)
+    {
+        d_out[blockIdx.x] = sdata[0];
+    }
+}
+
+__global__ void find_max_kernel(const float* const d_in, float* d_out)
+{
+    // sdata is allocated in the kernel call: 3rd arg to <<<b, t, shmem>>>
+    extern __shared__ float sdata[];
+
+    int myId = threadIdx.x + blockDim.x * blockIdx.x;
+    int tid  = threadIdx.x;
+
+    // load shared mem from global mem
+    sdata[tid] = d_in[myId];
+    __syncthreads();            // make sure entire block is loaded!
+
+    // do reduction in shared mem
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1)
+    {
+        if (tid < s)
+        {
+            sdata[tid] = max(sdata[tid], sdata[tid + s]);
+        }
+        __syncthreads();        // make sure all adds at one stage are done!
+    }
+
+    // only thread 0 writes result for this block back to global mem
+    if (tid == 0)
+    {
+        d_out[blockIdx.x] = sdata[0];
+    }
+}
+
+void find_min_max(const size_t numPixels, const float* const d_logLuminance, float &min_logLum, float &max_logLum)
+{
+  const int maxThreadsPerBlock = 256;
+  int threads = maxThreadsPerBlock;
+  int blocks = numPixels / maxThreadsPerBlock;
+  int threads2 = blocks;
+  int blocks2 = 1;
+
+  float* d_out = 0;
+  float* d_min = 0;
+  float* d_max = 0;
+  checkCudaErrors(cudaMalloc(&d_out, blocks * sizeof(float)));
+  checkCudaErrors(cudaMalloc(&d_min, sizeof(float)));
+  checkCudaErrors(cudaMalloc(&d_max, sizeof(float)));
+
+
+  find_min_kernel<<<blocks, threads, threads * sizeof(float)>>>(d_logLuminance, d_out);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  find_min_kernel<<<blocks2, threads2, threads2 * sizeof(float)>>>(d_out, d_min);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  find_max_kernel<<<blocks, threads, threads * sizeof(float)>>>(d_logLuminance, d_out);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  find_max_kernel<<<blocks2, threads2, threads2 * sizeof(float)>>>(d_out, d_max);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  checkCudaErrors(cudaMemcpy(&min_logLum, d_min, sizeof(float), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(&max_logLum, d_max, sizeof(float), cudaMemcpyDeviceToHost));
+
+  checkCudaErrors(cudaFree(d_out));
+  checkCudaErrors(cudaFree(d_min));
+  checkCudaErrors(cudaFree(d_max));
+}
+
+
+
+__global__ void log_histogram_kernel(const size_t numPixels, 
+                                     const float* const d_in, 
+                                     unsigned int* d_bins, 
+                                     const float min_logLum,
+                                     const float range, 
+                                     const size_t numBins)
+{
+  int myId = threadIdx.x + blockDim.x * blockIdx.x;
+  if (myId >= numPixels)
+  {
+    return;
+  }
+
+  unsigned int myBin = min(numBins-1, size_t(numBins * (d_in[myId] - min_logLum) / range));
+  atomicAdd(&(d_bins[myBin]), 1);
+}
+
+__global__ void hillis_steele_scan_inc_kernel(const size_t numBins, int step, unsigned int* d_bins_tmp1, unsigned int* const d_bins_tmp2)
+{
+  int myId = threadIdx.x + blockDim.x * blockIdx.x;
+  if (myId + step >= numBins)
+  {
+    return;
+  }
+  d_bins_tmp2[myId + step] = d_bins_tmp1[myId] + d_bins_tmp1[myId + step];
+}
+
+void hillis_steele_scan_exc(const size_t numBins, unsigned int* d_bins, unsigned int* const d_cdf)
+{
+  const int maxThreadsPerBlock = 256;
+  int threads = maxThreadsPerBlock;
+  int blocks = numBins / maxThreadsPerBlock;
+
+  unsigned int* d_bins_tmp1 = 0;
+  unsigned int* d_bins_tmp2 = 0;
+  checkCudaErrors(cudaMalloc(&d_bins_tmp1, numBins * sizeof(unsigned int)));
+  checkCudaErrors(cudaMalloc(&d_bins_tmp2, numBins * sizeof(unsigned int)));
+  checkCudaErrors(cudaMemcpy(d_bins_tmp1, d_bins, numBins * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+  checkCudaErrors(cudaMemcpy(d_bins_tmp2, d_bins, numBins * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+
+
+  int step = 1;
+  while (step < numBins)
+  {
+    hillis_steele_scan_inc_kernel<<<blocks, threads>>>(numBins, step, d_bins_tmp1, d_bins_tmp2);
+    checkCudaErrors(cudaMemcpy(d_bins_tmp1, d_bins_tmp2, numBins * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+    checkCudaErrors(cudaGetLastError());
+    step *= 2;
+  }
+  
+  checkCudaErrors(cudaMemset(d_bins_tmp1, 0, numBins * sizeof(unsigned int)));
+  checkCudaErrors(cudaMemcpy(d_bins_tmp1+1, d_bins_tmp2, (numBins-1) * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+  checkCudaErrors(cudaMemcpy(d_cdf, d_bins_tmp1, numBins * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+}
+
+
+// void blelloch_scan_exc(const size_t numBins, unsigned int* d_bins, unsigned int* const d_cdf)
+// {
+//   const int maxThreadsPerBlock = 256;
+//   int threads = maxThreadsPerBlock;
+//   int blocks = numPixels / maxThreadsPerBlock;
+
+//   unsigned int* d_bins_tmp1 = 0, d_bins_tmp2 = 0;
+//   checkCudaErrors(cudaMalloc(&d_bins_tmp1, numBins * sizeof(unsigned int)));
+//   checkCudaErrors(cudaMalloc(&d_bins_tmp2, numBins * sizeof(unsigned int)));
+//   checkCudaErrors(cudaMemcpy(d_bins_tmp1, d_bins, numBins * sizeof(unsigned int), cudaDeviceToDevice));
+//   checkCudaErrors(cudaMemcpy(d_bins_tmp2, d_bins, numBins * sizeof(unsigned int), cudaDeviceToDevice));
+// }
+
 void your_histogram_and_prefixsum(const float* const d_logLuminance,
                                   unsigned int* const d_cdf,
                                   float &min_logLum,
@@ -100,5 +263,23 @@ void your_histogram_and_prefixsum(const float* const d_logLuminance,
        the cumulative distribution of luminance values (this should go in the
        incoming d_cdf pointer which already has been allocated for you)       */
 
+  size_t numPixels = numRows * numCols;
+  find_min_max(numPixels, d_logLuminance, min_logLum, max_logLum);
+  float range = max_logLum - min_logLum;
 
+  const int maxThreadsPerBlock = 256;
+  int threads = maxThreadsPerBlock;
+  int blocks = numPixels / maxThreadsPerBlock;
+
+  unsigned int* d_bins = 0;
+  checkCudaErrors(cudaMalloc(&d_bins, numBins * sizeof(unsigned int)));
+  checkCudaErrors(cudaMemset(d_bins, 0, numBins * sizeof(unsigned int)));
+
+  log_histogram_kernel<<<blocks, threads>>>(numPixels, d_logLuminance, d_bins, min_logLum, range, numBins);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+
+  hillis_steele_scan_exc(numBins, d_bins, d_cdf);
+//  checkCudaErrors(cudaMemcpy(d_cdf, d_bins, numBins * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+  checkCudaErrors(cudaFree(d_bins));
 }
